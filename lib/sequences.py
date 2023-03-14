3d3e846ee6a56a6a929bd14f5c865dcaefb3dbce
Sarah Whalley <s.whalley1130@gmail.com>
2023-03-14 16:02:33 -0600

    Completes lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+
+    if length==0:
+        print([])
+    elif length==1:
+        print([0])
+    else:
+        newList = [0,1]
+        for i in range(2, length):
+            newList.append(newList[i-1]+newList[i-2])
+        print(newList)
+
